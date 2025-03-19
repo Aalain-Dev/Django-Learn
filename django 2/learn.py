8f9ced4a0e1f2ddfac311c1aba23832fbfb9a4bd
Aalain-Dev <aalainsmst@gmail.com>
2025-03-19 12:01:34 +0530

    Created A New App + Added how to create a new app in lern.py

---
+++ django 2/learn.py
@@ -0,0 +1,10 @@
+How to setup new project in django
+
+1. here is command to create venv = python -m venv myenv
+2. then activate venv = myenv\Scripts\activate
+3. then install django = pip install django
+4. then create new project = django-admin startproject myproject
+5. navigate to project folder = cd myproject
+6. then create new app = python manage.py startapp myapp
+7. then run server (check for manage.py file using ls command) = python manage.py runserver
+8. then open browser and type = localhost:8000
